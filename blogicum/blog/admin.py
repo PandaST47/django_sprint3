45705d9fd0752a071625c63627824802daecea40
Panda_ST47 <global-gaming233@yandex.ru>
2025-03-22 23:52:45 +0500

    first_reply

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,26 @@
+# blog/admin.py
+
+from django.contrib import admin
+from .models import Category, Location, Post
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'is_published', 'created_at')
+    list_filter = ('is_published', )
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title', )}
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published', 'created_at')
+    list_filter = ('is_published', )
+    search_fields = ('name', )
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'author', 'category', 'is_published')
+    list_filter = ('is_published', 'category', 'author')
+    search_fields = ('title', 'text')
+    date_hierarchy = 'pub_date'
+    readonly_fields = ('created_at',)
---
+++ blogicum/blog/views.py
@@ -0,0 +1,53 @@
+# blog/views.py
+
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from blog.models import Post, Category
+
+def index(request):
+    current_time = timezone.now()
+
+
+    posts = Post.objects.filter(
+        pub_date__lte=current_time,
+        is_published=True,
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+
+    context = {'post_list': posts}
+    return render(request, 'blog/index.html', context)
+
+
+def post_detail(request, id):
+    current_time = timezone.now()
+
+    post = get_object_or_404(
+        Post,
+        pk=id,
+        is_published=True,
+        pub_date__lte=current_time,
+        category__is_published=True
+    )
+
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
+
+
+def category_posts(request, category_slug):
+    current_time = timezone.now()
+
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+
+    posts = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=current_time
+    ).order_by('-pub_date')
+
+    context = {'category': category, 'post_list': posts}
+    return render(request, 'blog/category.html', context)
